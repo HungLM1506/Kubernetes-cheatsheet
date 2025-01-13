7d57d94438ffd34fadb3a33481638fbe8bcaa669
HungLM1506 <leminhhung933@gmail.com>
2025-01-13 22:50:36 +0700

    Install worker kubernetes in node Ubuntu

--- ubuntu/worker.sh
+++ ubuntu/worker.sh
@@ -40,8 +40,9 @@ echo
 echo "**** install repository packages kubernetes ****"
 echo 
 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/k8s.list
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 
 echo 
